98b1024427ef40a730afa608964a711d7e2d5ab4
tienpj <tienpj@users.noreply.github.com>
2018-04-19 13:19:50 +0800

    學說話

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -52,34 +52,46 @@ class KamigoController < ApplicationController
     end
 
     def webhook
-        # 設定回覆文字
-        reply_text = keyword_reply(received_text)
-    
+        # 學說話
+        reply_text = learn(received_text)
+
+        # 關鍵字回覆
+        reply_text = keyword_reply(received_text) if reply_text.nil?
+
         # 傳送訊息到 line
         response = reply_to_line(reply_text)
-        
+
         # 回應 200
         head :ok
     end 
-    
-      # 取得對方說的話
+   
+    # 取得對方說的話
     def received_text
         message = params['events'][0]['message']
         message['text'] unless message.nil?
     end
+
+    # 學說話
+    def learn(received_text)
+    #如果開頭不是 卡米狗學說話; 就跳出
+    return nil unless received_text[0..6] == '卡米狗學說話;'
     
-      # 關鍵字回覆
+    received_text = received_text[7..-1]
+    semicolon_index = received_text.index(';')
+
+    # 找不到分號就跳出
+    return nil if semicolon_index.nil?
+
+    keyword = received_text[0..semicolon_index-1]
+    message = received_text[semicolon_index+1..-1]
+
+    KeywordMapping.create(keyword: keyword, message: message)
+    '好哦～好哦～'
+    end
+
+    # 關鍵字回覆
     def keyword_reply(received_text)
-        # 學習紀錄表
-        keyword_mapping = {
-          'QQ' => '神曲支援：https://www.youtube.com/watch?v=T0LfHEwEXXw&feature=youtu.be&t=1m13s',
-          '我難過' => '神曲支援：https://www.youtube.com/watch?v=T0LfHEwEXXw&feature=youtu.be&t=1m13s',
-          'BB' =>'https://www.youtube.com/watch?v=EURj64CXyH4&feature=youtu.be&t=12m07s',
-          'TPJ' =>'87'
-        }
-        
-        # 查表
-        keyword_mapping[received_text]
+        KeywordMapping.where(keyword: received_text).last&.message
     end
     
       # 傳送訊息到 line
